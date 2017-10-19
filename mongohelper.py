d8e46bd40f9b72c4f10c999493981cb4cbcfa5e4
gsazheniuk <gsazheniuk@gmail.com>
2017-10-19 18:59:26 -0400

    Added default return in case connection is unavailable.

--- mongohelper.py
+++ mongohelper.py
@@ -20,5 +20,23 @@ class MongoHelper:
             for u in self.ufos.find():
                 ufos.append(u)
         except errors.ServerSelectionTimeoutError:
+            ufos.append({
+                "_id": 0,
+                "probability": 45000,
+                "max_crew_level": 1,
+                "diameter": 3,
+                "ufo_type": "Probe",
+                "max_structure": 100,
+                "max_loot": 3,
+                "bounty": 100,
+                "max_armor": 100,
+                "min_loot": 1,
+                "min_crew": 1,
+                "max_crew": 1,
+                "duration": 25,
+                "max_instances_in_sector": 10,
+                "max_shields": 100,
+                "max_speed": 100
+            })
             pass
         return ufos

77e9aa9177ed1861d2f86c4fb14010443c45d57f
gsazheniuk <gsazheniuk@gmail.com>
2017-10-25 13:24:08 -0400

    Added action for first appearance of UFO on the map

--- Tools/mongohelper.py
+++ Tools/mongohelper.py
@@ -38,6 +38,7 @@ class MongoHelper:
                 "max_shields": 100,
                 "max_speed": 100,
                 "actions": [
+                    {"name": "appear", "probability": 0, "max_duration": 5, "status": 0},
                     {"name": "move", "probability": 6, "max_duration": 10, "status": 1},
                     {"name": "leave", "probability": 4, "max_duration": 15, "status": 0},
                 ]

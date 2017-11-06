9ef6757c40b7285328d827862f2de0e1d4cff820
gsazheniuk <gsazheniuk@gmail.com>
2017-11-05 21:51:00 -0500

    Changed actions from list to dictionary.

--- Tools/mongohelper.py
+++ Tools/mongohelper.py
@@ -37,11 +37,12 @@ class MongoHelper:
                 "max_instances_in_sector": 10,
                 "max_shields": 100,
                 "max_speed": 100,
-                "actions": [
-                    {"name": "appear", "probability": 0, "max_duration": 5, "status": 0},
-                    {"name": "move", "probability": 6, "max_duration": 10, "status": 1},
-                    {"name": "leave", "probability": 4, "max_duration": 15, "status": 0},
-                ]
+                "acceleration": 15,
+                "actions": {
+                    "appear": {"probability": 0, "max_duration": 5, "status": 0},
+                    "move": {"probability": 6, "max_duration": 10, "status": 1},
+                    "leave": {"probability": 4, "max_duration": 15, "status": 0},
+                }
             })
             pass
         return ufos

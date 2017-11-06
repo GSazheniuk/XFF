0e5087ebe90d56cb58b5872f6b84bf686bc2e34e
gsazheniuk <gsazheniuk@gmail.com>
2017-11-05 21:50:12 -0500

    Added actions.

--- MongoCollections/UFOs.py
+++ MongoCollections/UFOs.py
@@ -5,7 +5,7 @@ class Probe:
     def __init__(self):
         self._id = 0
         self.ufo_type = 'Probe'
-        self.probability = 45000
+        self.probability = 125000
         self.duration = 50                      # Ticks
         self.max_instances_in_sector = 10
         self.diameter = 3
@@ -19,6 +19,12 @@ class Probe:
         self.min_loot = 1
         self.max_loot = 3
         self.bounty = 100
+        self.acceleration = 15
+        self.actions = {
+            "appear": {"probability": 0, "max_duration": 5, "status": 0},
+            "move": {"probability": 6, "max_duration": 10, "status": 1},
+            "leave": {"probability": 4, "max_duration": 15, "status": 0},
+        }
         pass
 
     def to_json(self):
@@ -51,6 +57,12 @@ class SmallScout:
         self.min_damage = 0
         self.max_damage = 0
         self.accuracy = 0
+        self.acceleration = 25
+        self.actions = {
+            "appear": {"probability": 0, "max_duration": 5, "status": 0},
+            "move": {"probability": 6, "max_duration": 10, "status": 1},
+            "leave": {"probability": 4, "max_duration": 15, "status": 0},
+        }
         pass
 
     def to_json(self):
@@ -83,6 +95,12 @@ class MediumScout:
         self.min_damage = 5
         self.max_damage = 20
         self.accuracy = 40
+        self.acceleration = 25
+        self.actions = {
+            "appear": {"probability": 0, "max_duration": 5, "status": 0},
+            "move": {"probability": 6, "max_duration": 10, "status": 1},
+            "leave": {"probability": 4, "max_duration": 15, "status": 0},
+        }
         pass
 
     def to_json(self):
@@ -115,6 +133,12 @@ class LargeScout:
         self.min_damage = 25
         self.max_damage = 40
         self.accuracy = 60
+        self.acceleration = 25
+        self.actions = {
+            "appear": {"probability": 0, "max_duration": 5, "status": 0},
+            "move": {"probability": 6, "max_duration": 10, "status": 1},
+            "leave": {"probability": 4, "max_duration": 15, "status": 0},
+        }
         pass
 
     def to_json(self):

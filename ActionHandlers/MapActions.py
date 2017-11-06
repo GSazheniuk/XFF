7570b26fce897c5189a44d2599eeefb1faecb76f
gsazheniuk <gsazheniuk@gmail.com>
2017-11-05 21:49:38 -0500

    Added 'move', 'appear' and 'disappear' actions.

--- ActionHandlers/MapActions.py
+++ ActionHandlers/MapActions.py
@@ -32,3 +32,80 @@ def approach_to_object(**kwargs):
         return 1
 
     return 0
+
+
+def move_to_point(**kwargs):
+    obj = None
+    point = None
+
+    if "object" in kwargs:
+        obj = kwargs["object"]
+    if "point" in kwargs:
+        point = kwargs["point"]
+
+    if point is None or obj is None:
+        print("move_to_point error: point or object is None!")
+        return 0
+
+    mo = obj.map_object
+    o = obj.data
+
+    if "speed" not in o:
+        o["speed"] = 0
+
+    if o["speed"] < o["max_speed"]:
+        o["speed"] = HelperFunctions.minimum(o["speed"] + o["acceleration"], o["max_speed"])
+
+    dist = HelperFunctions.distance(mo.X, mo.Y, point["X"], point["Y"])
+    print('Distance to point: ', dist)
+    k = o["speed"] / dist
+    mo.X += round((point["X"]-mo.X)*k)
+    mo.Y += round((point["Y"]-mo.Y)*k)
+    obj.duration -= 1
+
+    if dist <= o["speed"] or obj.duration == 0:
+        obj.Status = config.PlayerStatuses.PLAYER_STATUS_MOVING
+        return 1
+
+    print(obj.id, ' moving for next ', obj.duration, ' ticks')
+    return 0
+
+
+def appear(**kwargs):
+    obj = None
+
+    if "object" in kwargs:
+        obj = kwargs["object"]
+
+    if obj is None:
+        print('appear error: object is None!')
+        return 0
+
+    obj.duration -= 1
+
+    if obj.duration == 0:
+        obj.Status = config.PlayerStatuses.PLAYER_STATUS_MOVING
+        return 1
+
+    print(obj.id, ' appears in ', obj.duration, ' ticks')
+    return 0
+
+
+def disappear(**kwargs):
+    obj = None
+
+    if "object" in kwargs:
+        obj = kwargs["object"]
+
+    if obj is None:
+        print('disappear error: object is None!')
+        return 0
+
+    obj.duration -= 1
+
+    if obj.duration == 0:
+        obj.Status = config.PlayerStatuses.PLAYER_STATUS_MOVING
+        return -1
+
+    print(obj.id, ' leaves in ', obj.duration, ' ticks')
+    return 0

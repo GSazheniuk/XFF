07eba9f4091729bc580bf4245b6a72bf560cfa2e
gsazheniuk <gsazheniuk@gmail.com>
2017-11-06 21:32:28 -0500

    Added enums class.

--- static/js/tools.js
+++ static/js/tools.js
@@ -1,3 +1,7 @@
 function distance(aX, aY, bX, bY){
     return +(Math.sqrt(Math.pow(bX - aX, 2) + Math.pow(bY - aY, 2))).toFixed(2);
 }
+
+enums = {
+    UfoStatuses: ['Appearing', 'Moving', 'Leaving', 'Engaging', 'Fleeing', 'Crashed']
+}

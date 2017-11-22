89e8f8821de279ec7dfd0c96b832b4e7e93d6e43
gsazheniuk <gsazheniuk@gmail.com>
2017-11-21 22:46:57 -0500

    Re-organized skills class and default list.

--- MongoCollections/Skills.py
+++ MongoCollections/Skills.py
@@ -3,39 +3,31 @@ import config
 
 
 class Skill:
-    def __init__(self, name, cost, reqs):
+    def __init__(self, name, cost, reqs, skill_group, prim_attr, sec_attr, _id):
         self.Name = name
         self.Cost = cost
         self.Reqs = reqs
         self.Level = 0
         self.Progress = 0
+        self.SkillGroup = skill_group
+        self.PrimaryAttribute = prim_attr
+        self.SecondaryAttribute = sec_attr
         self.Status = config.SkillStatuses.SKILL_AVAILABLE if len(reqs) == 0 else config.SkillStatuses.SKILL_UNAVAILABLE
+        self._id = _id
         pass
 
-
-class BaseSkills:
-    def __init__(self):
-        self._id = 1
-        self.GroupName = 'Base Maintenance'
-        self.Skills = []
-        self.Skills.append(Skill('Construction', 300, []))
-        self.Skills.append(Skill('Advanced Construction', 500, {'Construction': 2}))
-        pass
-
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
+    def __str__(self):
+        return str(self.__dict__)
 
 
-class TechSkills:
-    def __init__(self):
-        self._id = 2
-        self.GroupName = 'Research'
-        self.Skills = []
-        self.Skills.append(Skill('Basic Research', 300, []))
-        self.Skills.append(Skill('Advanced Research', 500, {'Basic Research': 2}))
-        pass
-
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
+def default_skills():
+    skills = list()
+    skills.append(Skill('Construction', 300, [], 'Base Maintenance', 'Endurance', 'Charisma', 1))
+    skills.append(
+        Skill('Advanced Construction', 500, [{'Construction': 2}], 'Base Maintenance', 'Endurance', 'Charisma', 2)
+    )
+    skills.append(Skill('Basic Research', 300, [], 'Research', 'Intellect', 'Memory', 3))
+    skills.append(
+        Skill('Advanced Research', 500, [{'Basic Research': 2}], 'Research', 'Intellect', 'Memory', 4)
+    )
+    return skills

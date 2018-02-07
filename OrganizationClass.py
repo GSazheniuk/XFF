3291dd340a08a8c67b0bdacede6dccd80a703431
gsazheniuk <gsazheniuk@gmail.com>
2018-02-06 21:57:20 -0500

    Add Headquarters for Organization

--- OrganizationClass.py
+++ OrganizationClass.py
@@ -1,6 +1,18 @@
+from Bunker import Bunker
+import SharedData
+
+
 class Organization:
     def __init__(self, name):
         self.Name = name
+        self.Bases = []
+        hq = Bunker(
+            name="%s Headquarters" % self.Name,
+            organization=self,
+            sector=SharedData.Map.DefaultSector
+        )
+        self.Bases.append(hq)
+        SharedData.AllBases[hq.get_id()] = hq
         pass
 
     def toJSON(self):

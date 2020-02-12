b15713e6d70b9a19ee1db36ca151b060c7495eef
gsazheniuk <gsazheniuk@gmail.com>
2020-02-11 23:01:47 -0500

    Default actions for Ground Site

--- MongoCollections/GroundSite.py
+++ MongoCollections/GroundSite.py
@@ -9,12 +9,17 @@ class BaseSite:
 
 class BasicSectoidOperation(BaseSite):
     def __init__(self):
-        self._id = 0
+        self._id = 101
         self.site_type = "Basic Sectoid Operation"
         self.min_enemies = 3
         self.max_enemies = 5
         self.encounter_types = ["Sectoid"]
         self.probability = 25000
+        self.actions = {
+            "appear": {"probability": 0, "max_duration": 5, "status": 0},
+            "stay": {"probability": 8, "max_duration": 12, "status": 1},
+            "leave": {"probability": 2, "max_duration": 15, "status": 0},
+        }
         pass
 
     def get_id(self):

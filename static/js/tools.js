354cb90791b7ef4f7880c44111a9e095f6cabb5c
gsazheniuk <gsazheniuk@gmail.com>
2020-10-30 23:11:13 -0400

    Moved listen2queue func to tools.js

--- static/js/tools.js
+++ static/js/tools.js
@@ -2,6 +2,13 @@ function distance(aX, aY, bX, bY){
     return +(Math.sqrt(Math.pow(bX - aX, 2) + Math.pow(bY - aY, 2))).toFixed(2);
 }
 
+function listen2queue(endpoint, callback){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", endpoint, true);
+        xhr.onprogress = callback;
+        xhr.send();
+}
+
 enums = {
     UfoStatuses: ['Appearing', 'Moving', 'Leaving', 'Engaging', 'Fleeing', 'Crashed'],
     SkillStatuses: {
